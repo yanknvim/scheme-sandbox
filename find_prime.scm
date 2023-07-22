2d26b249848c1992ae38c15214a09d3cd3b5a0eb
yanknvim <yanknvim@gmail.com>
2023-07-22 09:22:18 +0900

    関数を整理

--- find_prime.scm
+++ find_prime.scm
@@ -4,9 +4,11 @@
 		(cons a (make-list (+ a 1) b))))
 
 (define (find-prime lst x)
+	(define (filter-not-prime lst prime)
+		(filter (lambda (x) (not (= (modulo x prime) 0))) lst))
 	(let ((prime (car lst)))
 		(if (> prime (sqrt x))
-			(cons prime (filter (lambda (x) (not (= (modulo x prime) 0))) lst))
-			(cons prime (find-prime (filter (lambda (x) (not (= (modulo x prime) 0))) lst) x)))))
+			(cons prime (filter-not-prime lst prime))
+			(cons prime (find-prime (filter-not-prime lst prime) x)))))
 
 (display (find-prime (make-list 2 100) 100))
